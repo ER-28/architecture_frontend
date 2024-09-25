0ea07484d7e8e49887f75459b9990ce497550ee8
l.guerra--rago <l.guerra--rago@xefi.fr>
2024-09-25 17:13:42 +0200

    wip

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {createBrowserRouter, RouteObject, RouterProvider} from "react-router-dom";
+
+
+
+function App({routes}: { routes:RouteObject[] }) {
+
+  return (
+    <RouterProvider router={createBrowserRouter(routes)} />
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import {importAllRoutes} from "./core/routing/bootstrapping.ts";
+
+importAllRoutes().then((routes) => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App routes={routes}/>
+    </StrictMode>,
+  )
+})
