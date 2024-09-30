e32fd62799420c39b299ebd35b3bf0a3a25a96a1
l.guerra--rago <l.guerra--rago@xefi.fr>
2024-09-30 11:48:09 +0200

    add some vite plugins

--- src/api/user.ts
+++ src/api/user.ts
@@ -9,8 +9,6 @@ export const createUser = apiWrapper.executeMutation<never, User>(
   apiWrapper.apiPost<never, User>('/users')
 )
 
-export const user = await apiWrapper.fetchQuery(['user', '123'], () => apiWrapper.apiGet<User>('/users/123'));
-
 export const updateUser = apiWrapper.executeMutation<User, never>(
   apiWrapper.apiPut<User, User>('/users/:id')
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import {VitePWA} from "vite-plugin-pwa";
+import { mdx } from "@cyco130/vite-plugin-mdx";
+import {ViteImageOptimizer} from "vite-plugin-image-optimizer";
+import {robots} from "vite-plugin-robots";
+import viteBundleObfuscator from "vite-plugin-bundle-obfuscator";
+import TurboConsole from 'unplugin-turbo-console/vite'
 
-export default defineConfig({
-  plugins: [react()],
-});
+const obfuscatorConfig = {
+  enable: true,
+  log: true,
+  autoExcludeNodeModules: true,
+  excludes: [],
+  options: {}
+};
+
+export default defineConfig(() => ({
+  plugins: [
+    mdx(),
+    react(),
+    VitePWA(),
+    ViteImageOptimizer(),
+    robots(),
+    viteBundleObfuscator(obfuscatorConfig),
+    TurboConsole()
+  ],
+}));
