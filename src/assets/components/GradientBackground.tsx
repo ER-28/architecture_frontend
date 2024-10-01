12277917dfdd0c404905385744ea8bd053c5cccd
l.guerra--rago <l.guerra--rago@xefi.fr>
2024-10-01 12:55:04 +0200

    dynamic background

---
+++ src/assets/components/GradientBackground.tsx
@@ -0,0 +1,55 @@
+import type React from "react";
+import type { ReactNode } from "react";
+import backgroundSvg from "../img/background.svg?raw";
+
+interface GradientBackgroundProps {
+	startColor: string;
+	endColor: string;
+	children: ReactNode;
+}
+
+const GradientBackground: React.FC<GradientBackgroundProps> = ({
+	startColor,
+	endColor,
+	children,
+}) => {
+	const replacedSvg = backgroundSvg.replace(/#C22D2A/g, startColor).replace(/#F5D50E/g, endColor)
+
+	return (
+		<div
+			style={{
+				position: "relative",
+				width: "100vw",
+				height: "100vh",
+				overflow: "hidden",
+			}}
+		>
+			<div
+				dangerouslySetInnerHTML={{ __html: replacedSvg }}
+				style={{
+					position: "absolute",
+					top: 0,
+					left: 0,
+					width: "100%",
+					height: "100%",
+					zIndex: -1,
+				}}
+			/>
+			<div
+				style={{
+					position: "relative",
+					zIndex: 1,
+					width: "100%",
+					height: "100%",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+				}}
+			>
+				{children}
+			</div>
+		</div>
+	);
+};
+
+export default GradientBackground;
--- vite.config.ts
+++ vite.config.ts
@@ -32,5 +32,6 @@ export default defineConfig(() => ({
 	],
 	server: {
 		host: true
-	}
+	},
+	assetsInclude: ['**/*.svg'],
 }));
