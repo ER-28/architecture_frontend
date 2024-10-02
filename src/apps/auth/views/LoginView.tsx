bb8fb6d0c36f30f2339c603446e874d64e69baa8
l.guerra--rago <l.guerra--rago@xefi.fr>
2024-10-02 13:52:56 +0200

    add favicon

--- src/apps/auth/views/LoginView.tsx
+++ src/apps/auth/views/LoginView.tsx
@@ -2,6 +2,7 @@ import { EyeIcon, EyeOffIcon } from "lucide-react";
 import { observer } from "mobx-react-lite";
 import type React from "react";
 import { useState } from "react";
+import Loading from "../../../Loading.tsx";
 import GradientBackground from "../../../assets/components/GradientBackground";
 import type { PortalConfigurationViewModel } from "../../portail/viewmodels/PortalConfigurationViewModel.ts";
 import type { LoginViewModel } from "../viewmodels/LoginViewModel";
@@ -18,7 +19,7 @@ const LoginView: React.FC<LoginViewProps> = observer(
 		const config = portalConfigViewModel.portalConfiguration;
 
 		if (!config) {
-			return <div>Loading...</div>; // Or a more sophisticated loading component
+			return <Loading />;
 		}
 
 		const handleSubmit = (e: React.FormEvent) => {
@@ -26,39 +27,40 @@ const LoginView: React.FC<LoginViewProps> = observer(
 			viewModel.login();
 		};
 
-		// TODO: Add Microsoft login functionality
-		// const loggingWithMicrosoft = (e: React.FormEvent) => {
-		// 	e.preventDefault();
-		// 	viewModel.loginWithMicrosoft();
-		// };
-
-		console.log(config.logo_url);
-
 		return (
 			<GradientBackground startColor="#eb4034" endColor="#c2158b">
-				<div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md">
-					<div className="flex justify-center mb-8">
-						<img
-							src={config.logo_url}
-							alt={`${config.company_name} logo`}
-							className="h-16 max-w-full"
+				<div className="bg-white rounded-lg shadow-lg w-full max-w-md">
+					<div className="relative pb-12">
+						<div className="px-8 pt-4 bg-gray-800 rounded-t-lg">
+							<div className="flex justify-center">
+								<img
+									draggable={false}
+									src={config.logo_url}
+									alt={`${config.company_name} logo`}
+									className="h-16 max-w-full"
+								/>
+							</div>
+							<h1 className="text-2xl font-bold text-center text-white mt-2">
+								{config.company_name}
+							</h1>
+						</div>
+						<div
+							className="absolute bottom-0 left-0 right-0 h-12 bg-gray-800"
+							style={{
+								borderBottomLeftRadius: "50% 100%",
+								borderBottomRightRadius: "50% 100%",
+							}}
 						/>
 					</div>
-					<h1 className="text-2xl font-bold text-center text-white mb-8">
-						{config.company_name}
-					</h1>
-					<form onSubmit={handleSubmit}>
+					<form onSubmit={handleSubmit} className="p-8">
 						<div className="mb-4">
-							<label
-								htmlFor="email"
-								className="block text-sm font-medium text-gray-400 mb-1"
-							>
+							<label htmlFor="email" className="block text-sm font-medium mb-1">
 								Email*
 							</label>
 							<input
 								type="email"
 								id="email"
-								className="w-full px-3 py-2 bg-gray-700 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
+								className="w-full px-3 py-2 bg-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
 								placeholder="Renseignez votre adresse mail"
 								value={viewModel.email}
 								onChange={(e) => viewModel.setEmail(e.target.value)}
@@ -68,7 +70,7 @@ const LoginView: React.FC<LoginViewProps> = observer(
 						<div className="mb-4">
 							<label
 								htmlFor="password"
-								className="block text-sm font-medium text-gray-400 mb-1"
+								className="block text-sm font-medium mb-1"
 							>
 								Mot de passe*
 							</label>
@@ -76,7 +78,7 @@ const LoginView: React.FC<LoginViewProps> = observer(
 								<input
 									type={showPassword ? "text" : "password"}
 									id="password"
-									className="w-full px-3 py-2 bg-gray-700 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
+									className="w-full px-3 py-2 bg-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
 									placeholder="Renseignez votre mot de passe"
 									value={viewModel.password}
 									onChange={(e) => viewModel.setPassword(e.target.value)}
@@ -84,7 +86,7 @@ const LoginView: React.FC<LoginViewProps> = observer(
 								/>
 								<button
 									type="button"
-									className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400"
+									className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-800"
 									onClick={() => setShowPassword(!showPassword)}
 								>
 									{showPassword ? (
@@ -102,18 +104,18 @@ const LoginView: React.FC<LoginViewProps> = observer(
 									id="remember-me"
 									checked={viewModel.rememberMe}
 									onChange={(e) => viewModel.setRememberMe(e.target.checked)}
-									className="h-4 w-4 text-red-600 focus:ring-red-500 border-gray-300 rounded accent-red-600"
+									className="h-4 w-4 text-red-600 focus:ring-red-500 border-gray-300 rounded accent-red-600 cursor-pointer"
 								/>
 								<label
 									htmlFor="remember-me"
-									className="ml-2 block text-sm text-gray-400"
+									className="ml-2 block text-sm cursor-pointer"
 								>
 									Se souvenir de moi
 								</label>
 							</div>
 							<div className="text-sm">
 								<a
-									className="font-medium text-red-400 hover:text-red-300"
+									className="font-medium text-red-500 hover:text-red-600"
 									href={"/forgot-password"}
 								>
 									Mot de passe oublié ?
@@ -127,23 +129,8 @@ const LoginView: React.FC<LoginViewProps> = observer(
 							Connexion
 						</button>
 					</form>
-					{/* TODO: ADD Microsoft logging for portal */}
-					{/*<div className="mt-6">*/}
-					{/*	<button*/}
-					{/*		type={"button"}*/}
-					{/*		className="w-full border-gray-500 border-2 text-white py-2 px-4 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-800 flex items-center justify-center"*/}
-					{/*		onClick={loggingWithMicrosoft}*/}
-					{/*	>*/}
-					{/*		<img*/}
-					{/*			src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/2048px-Microsoft_logo.svg.png"*/}
-					{/*			alt="Microsoft logo"*/}
-					{/*			className="w-5 h-5 mr-2"*/}
-					{/*		/>*/}
-					{/*		Continuer avec Microsoft*/}
-					{/*	</button>*/}
-					{/*</div>*/}
-					<p className="mt-8 text-xs text-center text-gray-400">
-						<a href="/gdpr" className="hover:text-orange-500">
+					<p className="text-xs text-center text-gray-400 py-4">
+						<a href="/gdpr" className="hover:text-red-500">
 							Politique de confidentialité - RGPD
 						</a>
 					</p>
